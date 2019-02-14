2a0ca5538d024f18ed4bdf9c0514abfb000233b4
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-14 11:46:29 +0530

    Exercise 01
    Get output using user inputs

---
+++ Ex01.java
@@ -0,0 +1,22 @@
+
+package ex01;
+
+import java.util.Scanner;
+public class Ex01 {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter three numbers here:");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(" ");
+        
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+    }
+    
+}
